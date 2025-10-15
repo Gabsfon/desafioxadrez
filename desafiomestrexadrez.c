4627c0a8deb03d5a6e164839d79f4aafb8257667
Gabsfon <gabrielsaisse@gmail.com>
2025-10-15 18:26:21 -0300

    Add files via upload
    desafio mestre comentado

--- desafiomestrexadrez.c
+++ desafiomestrexadrez.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-void movimentoTorre(int casas){
+void movimentoTorre(int casas){                         // função recursiva do movimento da torre
     if (casas > 0){
         printf("Direita\n");
         movimentoTorre(casas - 1);
     }
 }
-void movimentoRainha(int casas){
+void movimentoRainha(int casas){                        // função recursiva do movimento da rainha
     if (casas > 0){
         printf("Esquerda\n");
         movimentoRainha(casas - 1);
     }
 }
-void movimentoBispo(int casas){
+void movimentoBispo(int casas){                         // função recursiva do movimento do bispo
     if (casas > 0){
         printf("Cima, Direita\n");
         movimentoBispo(casas -1);
@@ -21,9 +21,9 @@ void movimentoBispo(int casas){
 
 int main(){
 
-    int cavalo;
+    int cavalo;                                         // declaração da variável do cavalo
 
-    printf("---Torre--- \n");
+    printf("---Torre--- \n");                           // saída no programa das funções declaradas anteriormente
     movimentoTorre(5);
     printf("---Rainha--- \n");
     movimentoRainha(8);
@@ -32,7 +32,7 @@ int main(){
     printf("---Cavalo--- \n");
 
     cavalo = 1;
-        while (cavalo--)                        // estrutura de repetição do movimento do cavalo        
+        while (cavalo--)                                // estrutura de repetição do movimento do cavalo        
         {
             for (int i = 0; i < 2; i++){
                 printf("Cima\n");
